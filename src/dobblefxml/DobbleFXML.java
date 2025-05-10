74f5cb69bf4870625ad0a46730dfa30bb2f16741
g4tot4ku <cllppsatgb031105@gmail.com>
2025-05-10 17:14:46 +0100

    AAA

---
+++ src/dobblefxml/DobbleFXML.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package dobblefxml;
+
+/**
+ *
+ * @author cllpp
+ */
+public class DobbleFXML {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
